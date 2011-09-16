9509192a5db9bde02f85bd4d2b29149efca2b673
garner.pl@gmail.com <garner.pl@gmail.com@685dc685-ef45-cea4-e5b9-41201535adf9>
2011-09-16 00:38:20 +0000

    Creating the skeleton files for the project and adding them to version control.  Added a spreadsheet to the doc directory to hold observed data.

---
+++ autobiome/init.h
@@ -0,0 +1,9 @@
+#ifndef AUTOBIOME_INIT_H
+  #define AUTOBIOME_INIT_H
+
+
+unsigned
+init( void ) ;
+
+
+#endif  /* #ifndef AUTOBIOME_INIT_H */
---
+++ src/biome.cxx
@@ -0,0 +1,26 @@
+
+// Systemwide C Headers
+#include <avr/io.h>
+
+// Project Local C Headers
+#include "autobiome/init.h"
+
+
+//const   Baud_Register  baud_reg_val =  baud_38400 ;
+
+  
+int
+main()
+{
+  // init( baud_reg_val ) ;
+  init() ;
+
+  while( true )
+  {
+
+  } /* while */
+
+  return  0 ;
+
+} /* main() */
+
