e3b615f08cbf105e081947547ab655a8108ba398
garner.pl@gmail.com <garner.pl@gmail.com@685dc685-ef45-cea4-e5b9-41201535adf9>
2011-09-18 07:39:29 +0000

    Updated spreadsheet, need to line graph temperature / millivolts  as x,y

--- trunk/autobiome/init.h
+++ trunk/autobiome/init.h
@@ -2,8 +2,20 @@
   #define AUTOBIOME_INIT_H
 
 
+/* Systemwide C Headers */
+#include <avr/io.h>
+
+
+#define  LED_BIT  6
+#define  LED_DDR  DDRD
+#define  LED_PORT PORTD
+
+
 unsigned
 init( void ) ;
+
+void
+switch_power_led_on( void ) ;
 
 
 #endif  /* #ifndef AUTOBIOME_INIT_H */
