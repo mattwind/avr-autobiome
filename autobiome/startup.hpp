06bf742f785af229a186a9edd17cd03e7fafc28f
garner.pl@gmail.com <garner.pl@gmail.com@685dc685-ef45-cea4-e5b9-41201535adf9>
2011-10-04 03:14:16 +0000

    Dividing up the program into filenames indicative of the files purpose

---
+++ autobiome/startup.hpp
@@ -0,0 +1,15 @@
+#ifndef AUTOBIOME_STARTUP_HPP
+  #define AUTOBIOME_STARTUP_HPP
+
+
+namespace autobiome
+{
+
+
+comm_init() ;
+
+
+} /* namespace autobiome */
+
+
+#endif  /* #ifndef AUTOBIOME_STARTUP_HPP */
