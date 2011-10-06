78261248df83742faf778962224c2c37bd09dfbb
garner.pl@gmail.com <garner.pl@gmail.com@685dc685-ef45-cea4-e5b9-41201535adf9>
2011-10-06 23:09:24 +0000

    This is a working temperature logger now.  It will send raw ADC data (a value between 0 and 1023) to the serial port which can be displayed or logged via minicom.  The data being sent seems to make sense compared against a voltmetre.

---
+++ autobiome/startup.hxx
@@ -0,0 +1,19 @@
+#ifndef AUTOBIOME_STARTUP_HXX
+  #define AUTOBIOME_STARTUP_HXX
+
+
+namespace Autobiome
+{
+
+
+const Baud_Register     baud_rate = baud_38400 ;
+
+
+void
+comm_init() ;
+
+
+} /* namespace Autobiome */
+
+
+#endif  /* #ifndef AUTOBIOME_STARTUP_HXX */
