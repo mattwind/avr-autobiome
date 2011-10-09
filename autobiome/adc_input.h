1d180e59ca03427a2b2d49c27440486b8838f984
garner.pl@gmail.com <garner.pl@gmail.com@685dc685-ef45-cea4-e5b9-41201535adf9>
2011-10-09 06:09:09 +0000

    Writing a separate PC program to calculate the Steinhart Constants given the measured temperature resistance points for a thermistor.

---
+++ autobiome/adc_input.h
@@ -0,0 +1,16 @@
+#ifndef AUTOBIOME_ADC_INPUT_H
+  #define AUTOBIOME_ADC_INPUT_H
+
+
+extern "C"
+{
+
+
+uint16_t
+read_data() ;
+
+
+} /* extern "C" */
+
+
+#endif  /* #ifndef AUTOBIOME_ADC_INPUT_H */
